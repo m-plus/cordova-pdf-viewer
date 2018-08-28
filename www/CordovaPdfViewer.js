d6d4769cb1ee3ad5bb4be76e2397a30a16911dc1
Imam Asari <imam.asari@mplus.software>
2018-08-28 13:21:22 +0700

    fix position pdf and remove extenstion from title

--- www/CordovaPdfViewer.js
+++ www/CordovaPdfViewer.js
@@ -23,7 +23,7 @@ function onOrientationChange(e) {
         console.log('onOrientationChange error');
     };
 
-    exec(success, error, "CordovaPdfViewer", "redim", [rect.top, rect.left, rect.width, rect.height]);
+    exec(success, error, "CordovaPdfViewer", "redim", [rect.top + 20, rect.left, rect.width, rect.height]);
 }
 
 
@@ -34,7 +34,7 @@ exports.show = function(_viewerId, src, _title, success, error) {
 
     viewerId = _viewerId;
     documentSrc = src;
-    documentTitle = _title + '.pdf';
+    documentTitle = _title;
     var extension = src.split('.').pop();
 
     console.log('Source ' + src);
@@ -63,7 +63,7 @@ exports.show = function(_viewerId, src, _title, success, error) {
     isCurrentlyViewing = true;
     viewerElement = elem;
 
-    exec(success, error, "CordovaPdfViewer", "show", [src, documentTitle, rect.top, rect.left, rect.width, rect.height]);
+    exec(success, error, "CordovaPdfViewer", "show", [src, documentTitle, rect.top + 20, rect.left, rect.width, rect.height]);
 };
 
 exports.redim = function(success, error, top, left, width, height) {
